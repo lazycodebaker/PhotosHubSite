c6f56ce29501857a1a41e54cc75f67dbaa2f9e85
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-02-28 22:04:49 +0530

    .

---
+++ src/Helpers/Types&Interface.tsx
@@ -0,0 +1,5 @@
+
+export type WindowContextType = {
+    width: number;
+    height: number;
+};
---
+++ src/context/WindowContext.tsx
@@ -0,0 +1,32 @@
+
+import React, { useContext, createContext, useEffect, useState } from 'react';
+
+import { WindowContextType } from '@/Helpers/Types&Interface';
+
+const WindowContext = createContext<WindowContextType>({ width: 0, height: 0 });
+
+export const useWindowContext: () => WindowContextType = () => useContext(WindowContext);
+
+const WindowContextProvider : React.FC<{}> = ({ children } : React.PropsWithChildren) => {
+    const [width, setWidth] = useState<number>(0);
+    const [height, setHeight] = useState<number>(0);
+
+    const handleResize :() => void = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    };
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <WindowContext.Provider value={{ width, height }}>
+            {children}
+        </WindowContext.Provider>
+    );
+};
+
+export default WindowContextProvider;
