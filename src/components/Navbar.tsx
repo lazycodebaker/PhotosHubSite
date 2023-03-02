87cc8f34be3372939080c15c5014427f7a155031
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-02 13:11:02 +0530

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,7 +65,7 @@ export const Navbar: React.FC = () => {
                     <h1 className='text-gray-50 font-semibold text-md'>Join</h1>
                 </div>
 
-                <div className='cursor-pointer items-center justify-center mr-4 md:lg:mr-14 px-6 py-2 block sm:hidden rounded-md bg-black/50 backdrop-blur-sm'>
+                <div className='cursor-pointer items-center justify-center mr-4 md:lg:mr-14 px-4 py-2 block sm:hidden bars z-50 rounded-md bg-black/50 backdrop-blur-sm'>
                     <h1 className='text-gray-50 font-semibold text-md'><FaBars size={24} /></h1>
                 </div>
 
