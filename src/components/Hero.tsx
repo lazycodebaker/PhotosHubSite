cd4692ec96346a02b5d1ac602a357d880f866c7f
Anshuman Tiwari <lazycodebaker@outlook.com>
2023-03-03 17:10:34 +0530

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,11 @@
 export const Hero : React.FC = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
-            <img className='absolute top-0 left-0 -z-10 w-screen h-screen object-cover '
-            src={"https://cdn.wallpaperhub.app/cloudcache/3/e/e/2/2/a/3ee22a464c4eb2183e7816306ca2976bcd541d25.jpg"}
-            alt="random" />
+            <img 
+                className='absolute top-0 left-0 -z-10 w-screen h-screen object-fill'
+                src={"https://cdn.wallpaperhub.app/cloudcache/3/e/e/2/2/a/3ee22a464c4eb2183e7816306ca2976bcd541d25.jpg"}
+                alt="random" 
+            />
             <div className="absolute top-0 left-0 w-screen h-screen bg-gray-900 opacity-40"></div>
         </div>
     );
